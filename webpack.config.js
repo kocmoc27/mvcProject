b2046ad2c0eb19717b4cf926cbbc6ed6afa1821c
Nurlan <bnk40191@gmail.com>
2018-01-15 18:01:29 +0300

    add module class Module

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+    entry: './source/index',
+    output:{
+        path: path.resolve(__dirname,'public'),
+        filename:'app.js',
+        publicPath: '/js'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname,'public')
+    },
+
+    devtool: 'cheap-eval-source-map'
+};
